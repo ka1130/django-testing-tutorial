14714b3b8523c33493098e01e96a8c366a5dab55
Kamila Matla-Tomczyk <ka1130@o2.pl>
2020-05-31 22:06:27 +0200

    Add tests for the views

--- budgetproject/budget/views.py
+++ budgetproject/budget/views.py
@@ -6,9 +6,11 @@ from django.utils.text import slugify
 from .forms import ExpenseForm
 import json
 
+
 def project_list(request):
     project_list = Project.objects.all()
     return render(request, 'budget/project-list.html', {'project_list': project_list})
+
 
 def project_detail(request, project_slug):
     project = get_object_or_404(Project, slug=project_slug)
@@ -25,7 +27,8 @@ def project_detail(request, project_slug):
             amount = form.cleaned_data['amount']
             category_name = form.cleaned_data['category']
 
-            category = get_object_or_404(Category, project=project, name=category_name)
+            category = get_object_or_404(
+                Category, project=project, name=category_name)
 
             Expense.objects.create(
                 project=project,
@@ -35,9 +38,12 @@ def project_detail(request, project_slug):
             )
 
     elif request.method == 'DELETE':
-        id = json.loads(request.body)['id']
-        expense = Expense.objects.get(id=id)
-        expense.delete()
+        try:
+            id = json.loads(request.body)['id']
+            expense = Expense.objects.get(id=id)
+            expense.delete()
+        except:
+            return HttpResponse(status=404)
 
         return HttpResponse(status=204)
 
